46e45c15efd7995613e08006861fd606a3e84ce7
Evans Shango <evanshango@github.com>
2019-10-07 14:16:57 +0300

    implement map based services. closes #6

---
+++ pet-clinic-data/src/main/java/com/evans/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.evans.sfgpetclinic.services.map;
+
+import com.evans.sfgpetclinic.model.Vet;
+import com.evans.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
